d28ca5f303e8eca9146d59ce831fd1a48575986e
sitarnet <Sitar00557776>
2020-09-22 01:03:29 +0300

    Magento 2 Training : Unit 5 – Lesson C

---
+++ src/app/code/Sitarnet/Jobs/Controller/Adminhtml/Department/Delete.php
@@ -0,0 +1,55 @@
+<?php
+namespace Sitarnet\Jobs\Controller\Adminhtml\Department;
+
+use Magento\Backend\App\Action;
+
+class Delete extends Action
+{
+    protected $_model;
+
+    /**
+     * @param Action\Context $context
+     * @param \Sitarnet\Jobs\Model\Department $model
+     */
+    public function __construct(
+        Action\Context $context,
+        \Sitarnet\Jobs\Model\Department $model
+    ) {
+        parent::__construct($context);
+        $this->_model = $model;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Sitarnet_Jobs::department_delete');
+    }
+
+    /**
+     * Delete action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $id = $this->getRequest()->getParam('id');
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($id) {
+            try {
+                $model = $this->_model;
+                $model->load($id);
+                $model->delete();
+                $this->messageManager->addSuccess(__('Department deleted'));
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Exception $e) {
+                $this->messageManager->addError($e->getMessage());
+                return $resultRedirect->setPath('*/*/edit', ['id' => $id]);
+            }
+        }
+        $this->messageManager->addError(__('Department does not exist'));
+        return $resultRedirect->setPath('*/*/');
+    }
+}
